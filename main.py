7101a47f374bca91820bcaee1f1bdf1a47ccc182
Sam Artola <samanthaartola@gmail.com>
2023-11-28 14:40:03 -0500

    Working

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import base64
 import streamlit as st
 import requests
-
+import os
 
 st.title("WiTCON 2024 Registration")
 st.image("media/WiTCONLogo.png")
 st.header("Registration Form")
 
-AIRTABLE_PERSONAL_TOKEN = "patL4ThnOkhLmgKRa.2b010e7eb49f8b1a3f747362be874a861ee9b6f3e23f1b89bacf3b7392fe382a"
+AIRTABLE_PERSONAL_TOKEN = st.secrets["AIRTABLE_PERSONAL_TOKEN"]
 AIRTABLE_BASE_ID = "appV4sA3sAl1l8icn"
 AIRTABLE_TABLE_NAME = "tblAeVid7nYaMmShC"
 AIRTABLE_API_URL = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
@@ -17,46 +17,65 @@ headers = {
     'Content-Type': 'application/json',
 }
 
-with st.form("My form"):
-    firstName = st.text_input("Enter your first name:")
-    lastName = st.text_input("Enter your last name:")
-    email = st.text_input("Enter your university email:")
-    phone = st.text_input("Enter your phone number:")
-    resume = st.file_uploader("Upload your Resume here (PDF only!)", type="pdf")
-    pantherID = st.text_input("Enter Panther ID If Applicable:")
-    github = st.text_input("Please provide a link to your Github:")
-    linkedIn = st.text_input("Please provide the link to your LinkedIn:")
+firstName = st.text_input("Enter your first name:")
+lastName = st.text_input("Enter your last name:")
+email = st.text_input("Enter your university email:")
+phone = st.text_input("Enter your phone number:")
+resume = st.file_uploader("Upload your Resume here (PDF only!)", type="pdf")
+pantherID = st.text_input("Enter Panther ID If Applicable:")
+github = st.text_input("Please provide a link to your Github:")
+linkedIn = st.text_input("Please provide the link to your LinkedIn:")
 
-    academicStanding = st.selectbox("What is your academic standing?",
+academicStanding = st.selectbox("What is your academic standing?",
                                     index=None,
                                     options=["Freshman", "Sophomore", "Junior", "Senior", "Graduate"])
 
-    majorOptions = ["Computer Science BA", "Computer Science BS", "Computer Science MS", "Computer Science PhD",
+majorOptions = ["Computer Science BA", "Computer Science BS", "Computer Science MS", "Computer Science PhD",
                     "Cybersecurity BA", "Cybersecurity BS", "IoT", "Computer Engineering", "Electrical Engineering",
                     "Management Information Systems", "Information Technology BA", "Information Technology BS",
                     "Information Technology MS", "Information Technology PhD", "Data Science",
                     "Interdisciplinary Engineering", "Mechanical Engineering", "Biomedical Engineering",
                     "Civil Engineering", "Other"]
-    major = st.selectbox("What is your Major?",
+major = st.selectbox("What is your Major?",
                          options=majorOptions,
                          placeholder="Select Major",
                          index=None)
+if major == "Other":
+    major = st.text_input("Please enter your major:")
 
-    shirt = st.selectbox("What is your shirt size?",
+hispanic = st.checkbox("Do you identify as latino/latina/latinx?")
+
+ethnicities = ["African American/Black",
+                   "Caucasian/White",
+                   "Hispanic/Latino",
+                   "Asian",
+                   "Native American/Alaska Native",
+                   "Pacific Islander",
+                   "Middle Eastern/North African",
+                   "Biracial/Multiracial",
+                   "Other (please specify)",
+                   "Prefer not to say"]
+
+ethnicity = st.selectbox("What is your ethnicity?",
+                             options=ethnicities,
+                             placeholder="Select Ethnicity",
+                             index=None)
+
+shirt = st.selectbox("What is your shirt size?",
                          options=["S", "M", "L", "XL", "XXL"], index=None)
 
-    diet = st.multiselect("Any dietary restrictions?",
+diet = st.multiselect("Any dietary restrictions?",
                           options=["No", "Vegan", "Vegetarian", "Gluten Allergy", "Lactose Intolerance"])
 
-    consent = st.checkbox("I consent to the use of my likeness in media publications, for WiTCON and third parties, "
+consent = st.checkbox("I consent to the use of my likeness in media publications, for WiTCON and third parties, "
                           "on print and social media for the purpose of promotion and awareness."
                           )
 
-    pasta = st.text_input("What's your favorite pasta shape?")
-    st.image("https://sancarlo.co.uk/app/uploads/2019/12/pasta.jpg")
-    submitted = st.form_submit_button("Submit")
+pasta = st.text_input("What's your favorite pasta shape?")
+st.image("https://sancarlo.co.uk/app/uploads/2019/12/pasta.jpg")
+submitted = st.button("Submit")
 
-    if submitted:
+if submitted:
         st.write("Thank you for registering for WiTCON")
         form_entry = {
             'fields': {
@@ -74,7 +93,7 @@ with st.form("My form"):
             }
         }
 
-        temp=""
+        temp = ""
         for i in diet:
             temp += i
         form_entry['fields']['Diet'] = temp
